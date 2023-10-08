9faa7f9b9d5df4663567945674bcb0d7aec81222
emmylem <benjaminosondu55@gmail.com>
2023-10-08 16:49:20 +0000

    Prime Game update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,64 +2,41 @@
 """0. Prime Game - Maria and Ben are playing a game"""
 
 
-def is_winner(x: int, nums: list[int]) -> str | None:
-  """x - rounds
-  nums - numbers list
-  """
-  if x <= 0 or nums is None:
-    return None
-  if x != len(nums):
-    return None
+def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
 
-  # Create a sieve of Eratosthenes to store prime numbers.
-  sieve = [True] * (max(nums) + 1)
-  sieve[0] = sieve[1] = False
-  for i in range(2, int(max(nums) ** 0.5) + 1):
-    if sieve[i]:
-      for j in range(i * i, max(nums) + 1, i):
-        sieve[j] = False
+    ben = 0
+    maria = 0
 
-  # Count the number of prime numbers up to each number in the list.
-  prime_counts = [0] * len(nums)
-  for i in range(len(nums)):
-    for j in range(2, nums[i] + 1):
-      if sieve[j]:
-        prime_counts[i] += 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
-  # Calculate the score for each player.
-  ben_score = 0
-  maria_score = 0
-  for i in range(len(nums)):
-    if prime_counts[i] % 2 == 0:
-      ben_score += 1
-    else:
-      maria_score += 1
-
-  # Determine the winner.
-  if ben_score > maria_score:
-    return "Ben"
-  elif maria_score > ben_score:
-    return "Maria"
-  else:
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
     return None
 
 
-def remove_multiples(ls: list[int], x: int) -> None:
-  """removes multiple
-  of primes
-  """
-  for i in range(2, int(len(ls) ** 0.5) + 1):
-    if ls[i]:
-      for j in range(i * i, len(ls), i):
-        ls[j] = 0
-
-
-if __name__ == "__main__":
-  x = int(input("How many rounds? "))
-  nums = list(map(int, input("Enter the numbers: ").split()))
-
-  winner = is_winner(x, nums)
-  if winner is not None:
-    print(f"{winner} wins!")
-  else:
-    print("The game is a tie.")
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break

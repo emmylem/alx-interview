4a0eb20cc685090c4c277cc52f0b40956c4dc21d
emmylem <benjaminosondu55@gmail.com>
2023-10-28 20:34:26 +0100

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
+""" N queens """
 import sys
 
 
-def nqueens(N):
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    def is_valid(board, row, col):
-        for r, c in enumerate(board):
-            if c == col or r - c == row - col or r + c == row + col:
-                return False
-        return True
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    def solve(board, row):
-        if row == N:
-            print(board)
-            return
-        for col in range(N):
-            if is_valid(board, row, col):
-                board[row] = col
-                solve(board, row + 1)
-                board[row] = -1
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    solve([-1] * N, 0)
+n = int(sys.argv[1])
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    nqueens(sys.argv[1])
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
